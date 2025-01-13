f4a1fbac01aa1e5edcc077fc6b093a512930684e
UmerAalam <umerrazzaq.dev@gmail.com>
2025-01-13 22:42:18 +0500

    Change-folder-myapp

--- myapp/obj/Debug/net9.0/myapp.AssemblyInfo.cs
+++ myapp/obj/Debug/net9.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfead15e0927dfa11afb788ad06a48065f803e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
