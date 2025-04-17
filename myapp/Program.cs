5b0de9d74bbcfeff108821cc4372f85255c2d75b
UmerAalam <umerrazzaq.dev@gmail.com>
2025-04-17 23:08:03 +0500

    umer

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program 
+﻿using SFML.Graphics;
+
+class Program 
 {
     static void Main()
     {
--- myapp/scripts/Scene.cs
+++ myapp/scripts/Scene.cs
@@ -4,14 +4,14 @@ using SFML.Window;
 using SFML.System;
 public class Scene
 {
-    public Sketch sketch = new Sketch();
-    public Sketch sketch2 = new Sketch();
+    private Sketch sketch = new Sketch();
+    private Sketch sketch2 = new Sketch();
     public Collider collider = new Collider(new Vector2f[] { new Vector2f(0, 0), new Vector2f(100, 0), new Vector2f(100, 100), new Vector2f(0, 100) });
     public void Game()
     {
         sketch.Square(Color.Green,Vector2.Half);
         sketch.transform.Move(moveSpeed: 3f);
-        sketch2.Square(Color.Red);
+        sketch2.Circle(Color.Red);
         sketch2.transform.Location = new Vector2f(840, 360);
         if (sketch.Collider.IsColliding(sketch2.Collider))
         {
